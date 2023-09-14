14b7f24d9c17da8d2c48a2c746ff9531111f97be
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-09-14 11:01:32 -0700

    Initial Commit

---
+++ NWSWeather/NWSWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NWSWeatherApp.swift
+//  NWSWeather
+//
+//  Created by Jason Koehn on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NWSWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
